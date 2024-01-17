97b704f751e4134944142b7a3d29aa7430bbb70f
Kacper <skikac23@wp.pl>
2024-01-17 01:33:39 +0100

    Requesting product id then adding to cart

--- src/main/java/LoveWithLoft/LoveWithLoft/demo/DemoRestController.java
+++ src/main/java/LoveWithLoft/LoveWithLoft/demo/DemoRestController.java
@@ -1,6 +1,5 @@
 package LoveWithLoft.LoveWithLoft.demo;
 
-import LoveWithLoft.LoveWithLoft.user.User;
 import LoveWithLoft.LoveWithLoft.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,8 +14,6 @@ import java.security.Principal;
 @RequiredArgsConstructor
 public class DemoRestController {
 
-	private final UserRepository userRepository;
-
 	@GetMapping("/secure")
 	public ResponseEntity<String> sayHello(){
 		return ResponseEntity.ok("Hello form secured endpoint");
@@ -25,14 +22,5 @@ public class DemoRestController {
 	@GetMapping("/info")
 	public String getInfo(Principal principal){
 		return principal.toString();
-	}
-
-	@GetMapping("/me")
-	public ResponseEntity<User> getCurrentUserWithCart(Principal principal){
-		String email = principal.getName();
-
-		return userRepository.findByEmailWithCartItems(email)
-				.map(user -> ResponseEntity.ok().body(user))
-				.orElseGet(() -> ResponseEntity.notFound().build());
 	}
 }
--- src/main/java/LoveWithLoft/LoveWithLoft/user/UserRepository.java
+++ src/main/java/LoveWithLoft/LoveWithLoft/user/UserRepository.java
@@ -1,14 +1,16 @@
 package LoveWithLoft.LoveWithLoft.user;
 
+import LoveWithLoft.LoveWithLoft.cart.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	Optional<User> findByEmail(String email);
 
-	@Query("SELECT u FROM User u LEFT JOIN FETCH u.cartItems WHERE u.email = :email")
-	Optional<User> findByEmailWithCartItems(String email);
+	@Query("SELECT ci FROM User u JOIN u.cartItems ci WHERE u.email = :email")
+	Optional<List<CartItem>> findByEmailWithCartItems(String email);
 }
